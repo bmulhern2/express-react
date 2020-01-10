fbc06060b370aca9709be5256aec4424d142d0ad
bmulhern2 <bmulhern09@gmail.com>
2020-01-09 20:17:48 -0500

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 var app = express();
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/client/index.js');
+  res.sendFile(__dirname + '/client/App.js');
 });
 app.listen('5000');
 module.exports = app;
